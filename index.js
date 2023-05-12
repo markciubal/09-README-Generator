2e3e8812a029945dc9ee91fcceb05930d7cc1c0c
markciubal <mark.ciubal@gmail.com>
2023-05-12 12:17:37 -0700

    Finished inquirer write to file, and ask question functions.

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your first name?',
+        name: 'fname',
+    },
+    {
+        type: 'input',
+        message: 'What is your last name?',
+        name: 'lname',
+    },
+    {
+        type: 'input',
+        message: 'What is your location?',
+        name: 'location',
+    },
+    {
+        type: 'input',
+        message: 'What is your favorite color in hexidecimal?',
+        name: 'hex',
+    },
+    {
+        type: 'input',
+        message: 'What is your LinkedIn address?',
+        name: 'linkedin',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub address?',
+        name: 'github',
+    },
+];
+
+const askQuestions = () => {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            console.log(`Successfully inquirered.`);
+            const {fname, lname, location, hex, linkedin, github } = response;
+            console.log(questions);
+            const filename = `${fname}${lname}.txt`;
+            writeToFile(filename, JSON.stringify(questions));
+            console.log(`Successfully wrote ${filename}.`);
+        })
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+const writeToFile = (filename, data) => {
+    fs.writeFile(filename, data, function (error) {
+        if (error) {
+            console.error(error);
+        } else { 
+            console.log("Successfully wrote to file.");
+        }
+    })
+}
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => askQuestions(); 
 
 // Function call to initialize app
 init();
