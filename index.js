736bae011baadb5582f9c234c36b963f72a66236
markciubal <mark.ciubal@gmail.com>
2023-05-12 12:52:50 -0700

    generateMarkdown function is working.

--- index.js
+++ index.js
@@ -1,60 +1,80 @@
+/***
+ *    _____________________  _____  ________     _____  ___________                .___
+ *    \______   \_   _____/ /  _  \ \______ \   /     \ \_   _____/      _____   __| _/
+ *     |       _/|    __)_ /  /_\  \ |    |  \ /  \ /  \ |    __)_      /     \ / __ | 
+ *     |    |   \|        /    |    \|    `   /    Y    \|        \    |  Y Y  / /_/ | 
+ *     |____|_  /_______  \____|__  /_______  \____|__  /_______  / /\ |__|_|  \____ | 
+ *            \/        \/        \/        \/        \/        \/  \/       \/     \/ 
+ *               ________                                   __                         
+ *              /  _____/  ____   ____   ________________ _/  |_ ___________           
+ *             /   \  ____/ __ \ /    \_/ __ \_  __ \__  \\   __/  _ \_  __ \          
+ *             \    \_\  \  ___/|   |  \  ___/|  | \// __ \|  |(  <_> |  | \/          
+ *              \______  /\___  |___|  /\___  |__|  (____  |__| \____/|__|             
+ *                     \/     \/     \/     \/           \/                            
+
+https://patorjk.com/software/taag/#p=display&h=3&c=c&f=Graffiti&t=README.md%0A%20%20%20%20%20%20%20%20%20Generator
+ */
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        message: 'What is your first name?',
-        name: 'fname',
-    },
-    {
-        type: 'input',
-        message: 'What is your last name?',
-        name: 'lname',
-    },
-    {
-        type: 'input',
-        message: 'What is your location?',
-        name: 'location',
-    },
-    {
-        type: 'input',
-        message: 'What is your favorite color in hexidecimal?',
-        name: 'hex',
-    },
-    {
-        type: 'input',
-        message: 'What is your LinkedIn address?',
-        name: 'linkedin',
-    },
-    {
-        type: 'input',
-        message: 'What is your GitHub address?',
-        name: 'github',
-    },
-];
+const generateMarkdown = require('./utils/generateMarkdown');
 
 const askQuestions = () => {
+    // 
+    const questions = [
+        {
+            type: 'input',
+            message: 'What do you want to call this project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'What is your first name?',
+            name: 'fname',
+        },
+        {
+            type: 'input',
+            message: 'What is your last name?',
+            name: 'lname',
+        },
+        {
+            type: 'input',
+            message: 'What is your location?',
+            name: 'location',
+        },
+        {
+            type: 'input',
+            message: 'What is your favorite color in hexidecimal?',
+            name: 'hex',
+        },
+        {
+            type: 'input',
+            message: 'What is your LinkedIn address?',
+            name: 'linkedin',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub address?',
+            name: 'github',
+        },
+    ];
     inquirer
         .prompt(questions)
         .then((response) => {
-            console.log(`Successfully inquirered.`);
-            const {fname, lname, location, hex, linkedin, github } = response;
-            console.log(questions);
-            const filename = `${fname}${lname}.txt`;
-            writeToFile(filename, JSON.stringify(questions));
-            console.log(`Successfully wrote ${filename}.`);
+            const filename = `_README.md`;
+            writeToFile(filename, response);
+            console.log(`Successfully wrote to ${filename}.`);
         })
 };
 
 // TODO: Create a function to write README file
 const writeToFile = (filename, data) => {
-    fs.writeFile(filename, data, function (error) {
+    const readmeMarkdown = generateMarkdown(data);
+    fs.writeFile(filename, readmeMarkdown, function (error) {
         if (error) {
             console.error(error);
         } else { 
-            console.log("Successfully wrote to file.");
+            console.log(`Successfully wrote to ${filename}.`);
         }
     })
 }
