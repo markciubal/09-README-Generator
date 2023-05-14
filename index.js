aeb5e2bfd1894da4512883545dfd9cff915b25db
markciubal <mark.ciubal@gmail.com>
2023-05-14 16:50:39 -0700

    Working README generator with several parameters.

--- index.js
+++ index.js
@@ -11,7 +11,6 @@
  *             \    \_\  \  ___/|   |  \  ___/|  | \// __ \|  |(  <_> |  | \/          
  *              \______  /\___  |___|  /\___  |__|  (____  |__| \____/|__|             
  *                     \/     \/     \/     \/           \/                            
-
 https://patorjk.com/software/taag/#p=display&h=3&c=c&f=Graffiti&t=README.md%0A%20%20%20%20%20%20%20%20%20Generator
  */
 // TODO: Include packages needed for this application
@@ -24,11 +23,6 @@ const askQuestions = () => {
     const questions = [
         {
             type: 'input',
-            message: 'What do you want to call this project?',
-            name: 'title',
-        },
-        {
-            type: 'input',
             message: 'What is your first name?',
             name: 'fname',
         },
@@ -36,6 +30,21 @@ const askQuestions = () => {
             type: 'input',
             message: 'What is your last name?',
             name: 'lname',
+        },
+        {
+            type: 'input',
+            message: 'What do you want to call this project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'How would you describe your project?',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'How do you use your project?',
+            name: 'usage',
         },
         {
             type: 'input',
@@ -57,13 +66,30 @@ const askQuestions = () => {
             message: 'What is your GitHub address?',
             name: 'github',
         },
+        {
+            type: 'list',
+            message: 'What license is this?',
+            name: 'license',
+            choices: [
+                {
+                name: 'MIT License',
+                value: 'MIT',
+                description: 'A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.',
+                },
+                {
+                name: 'The Unlicense',
+                value: 'unlicense',
+                description: 'A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.yarn is an awesome package manager',
+                }
+            ]
+        },
     ];
     inquirer
         .prompt(questions)
         .then((response) => {
             const filename = `_README.md`;
             writeToFile(filename, response);
-            console.log(`Successfully wrote to ${filename}.`);
+            console.log(`Successfully inquirered.`);
         })
 };
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,86 @@
+// Create render functions for each section.
+const renderTitle = (title) => {
+  return `# ${title}
+  
+`;
+}
+
+const renderDescription = (description) => {
+  let descriptionMarkdown = `## Description
+${description}
+
+`
+  return descriptionMarkdown;
+}
+
+const renderTableOfContents = () => {
+  return `## Table of Contents
+- [Descrption](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+`;
+}
+
+const renderInstallation = () => {
+  return `## Installation
+npm install
+
+`;
+}
+
+const renderUsage = (usage) => {
+  return `## Usage
+${usage}
+
+`;
+}
+
+const renderCredits = (fname, lname) => {
+  return `## Credits
+Made by: ${fname} ${lname}
+
+`
+}
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = (license) => { 
 
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = (license) => {
 
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicense = (license) => {
+  switch (license) {
+    case 'MIT':
+      return `## License
+MIT
+
+`;
+      break;
+    default:
+      return "Other";
+      break;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  const {fname, lname, location, hex, linkedin, github } = data;
-  return `# ${fname}}
-
-`;
+  const { fname, lname, title, description, usage, location, hex, linkedin, github, license } = data;
+  let markdown = renderTitle(title);
+  markdown += renderTableOfContents();
+  markdown += renderDescription(description);
+  markdown += renderInstallation();
+  markdown += renderUsage(usage);
+  markdown += renderCredits(fname, lname);
+  markdown += renderLicense(license);
+  return markdown;
 }
 
 module.exports = generateMarkdown;
